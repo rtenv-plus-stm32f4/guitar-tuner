da200997bac96d485523c8f398829101baab281f
Shengwen Cheng <shengwen1997.tw@gmail.com>
2014-06-10 22:56:48 +0800

    Remove the built file

--- firmware/build/OS_kernel/src/memory-pool.o.d
+++
@@ -1,2 +0,0 @@
-build/OS_kernel/src/memory-pool.o: OS_kernel/src/memory-pool.c \
- OS_kernel/include/memory-pool.h
--- firmware/build/OS_kernel/src/task.o.d
+++
@@ -1,3 +0,0 @@
-build/OS_kernel/src/task.o: OS_kernel/src/task.c OS_kernel/include/task.h \
- OS_kernel/include/kconfig.h OS_kernel/include/list.h \
- OS_kernel/include/utils.h
