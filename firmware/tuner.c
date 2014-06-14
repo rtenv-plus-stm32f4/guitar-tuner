3eb378ca502d63156fee3a381e1fed6c1d611e09
LanKuDot <lankudot@github.com>
2014-06-14 21:22:08 +0800

    Using fft_1024 in tuner task

--- firmware/tuner.c
+++ firmware/tuner.c
@@ -1,7 +1,16 @@
 #include "tuner.h"
+#include "fft_1024.h"
+#include "buzz_C3.h"
+
+#define SAMPLE_RATE	44100
 
 void tuner_task()
 {
-	while(1) {
-	}
+	extern float test_signal[ FFT_DATA_LENGTH ];
+	float responseFreq;
+
+	responseFreq = fft_1024( test_signal, SAMPLE_RATE );
+	responseFreq += responseFreq;
+
+	while(1);
 }
