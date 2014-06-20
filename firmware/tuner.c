fde03bd3bde356e784e1ae0e1216939874d5f3c1
LanKuDot <lankudot@github.com>
2014-06-20 19:11:22 +0800

    Update the usage of fft_4096 and Makefile

--- firmware/tuner.c
+++ firmware/tuner.c
@@ -1,15 +1,15 @@
 #include "tuner.h"
-#include "fft_1024.h"
+#include "fft_4096.h"
 #include "buzz_C3.h"
 
-#define SAMPLE_RATE	44100
+#define SAMPLE_RATE	4096
 
 void tuner_task()
 {
-	extern float test_signal[ FFT_DATA_LENGTH ];
+	extern float test_signal[ DATA_LENGTH ];
 	float responseFreq;
 
-	responseFreq = fft_1024( test_signal, SAMPLE_RATE );
+	responseFreq = fft_4096( test_signal );
 	responseFreq += responseFreq;
 
 	while(1);
